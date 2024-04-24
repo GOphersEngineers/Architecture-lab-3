f39b7d58da9d5e3ca8d77477f3ac2557360499e5
Illya <katinskyi.illia@lll.kpi.ua>
2024-04-24 15:54:40 +0300

    Add letter T to the UI

--- ui/window.go
+++ ui/window.go
@@ -32,8 +32,8 @@ type Visualizer struct {
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	pw.pos.Max.X = 400
+	pw.pos.Max.Y = 400
 	driver.Main(pw.run)
 }
 
@@ -43,7 +43,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -133,7 +135,31 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	firstRectangle := image.Rectangle{
+		Min: image.Point{
+			X: pw.pos.Max.X - 200,
+			Y: pw.pos.Max.Y - 200,
+		},
+		Max: image.Point{
+			X: pw.pos.Max.X + 200,
+			Y: pw.pos.Max.Y - 100,
+		},
+	}
+	firstRectangleColor := color.RGBA{B: 255}
+	pw.w.Fill(firstRectangle, firstRectangleColor, draw.Src)
+
+	secondRectangle := image.Rectangle{
+		Min: image.Point{
+			X: pw.pos.Max.X - 50,
+			Y: pw.pos.Max.Y - 100,
+		},
+		Max: image.Point{
+			X: pw.pos.Max.X + 50,
+			Y: pw.pos.Max.Y + 200,
+		},
+	}
+	secondRectangleColor := color.RGBA{B: 255}
+	pw.w.Fill(secondRectangle, secondRectangleColor, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
